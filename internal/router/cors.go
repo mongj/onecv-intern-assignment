90261f59635b967d1983fc42cdd12fd7a85950ad
Ming Jun <evan.zhangmingjun@gmail.com>
2024-09-22 19:01:12 +0800

    chore: remove OPTIONS and PATCH methods from CORS

--- internal/router/cors.go
+++ internal/router/cors.go
@@ -9,7 +9,7 @@ import (
 func corsMiddleware() func(http.Handler) http.Handler {
 	options := cors.Options{
 		AllowedOrigins:   []string{"*"}, // Allow all origins for now since we don't have a frontend
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
 		AllowedHeaders:   []string{"Accept", "Content-Type", "X-CSRF-Token"},
 		AllowCredentials: true,
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
