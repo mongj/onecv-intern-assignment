b193c14e26fecd5b9402b9ecff53da47359e39ab
Ming Jun <evan.zhangmingjun@gmail.com>
2024-09-23 18:09:40 +0800

    feat: find eligible schemes by applicant

--- internal/handlers/schemes/find.go
+++ internal/handlers/schemes/find.go
@@ -1,7 +1,41 @@
 package schemes
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
 
-func HandleFind(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("find scheme"))
+	"github.com/google/uuid"
+	"github.com/mongj/gds-onecv-swe-assignment/internal/dataaccess"
+	"github.com/mongj/gds-onecv-swe-assignment/internal/handlers"
+	"github.com/mongj/gds-onecv-swe-assignment/internal/json"
+	"github.com/mongj/gds-onecv-swe-assignment/internal/middleware"
+	"github.com/mongj/gds-onecv-swe-assignment/internal/views"
+	"github.com/pkg/errors"
+)
+
+const findHandlerName = "schemes::findEligible"
+
+func HandleFind(w http.ResponseWriter, r *http.Request) ([]byte, int, error) {
+	db, err := middleware.GetDB(r)
+	if err != nil {
+		return nil, http.StatusInternalServerError, errors.Wrap(err, fmt.Sprintf(handlers.ErrGetDB, findHandlerName))
+	}
+
+	id, err := uuid.Parse(r.URL.Query().Get("applicant"))
+	if err != nil {
+		return nil, http.StatusBadRequest, errors.Wrap(err, "failed to parse applicant ID")
+	}
+
+	schemes, err := dataaccess.ListEligibleSchemes(db, id)
+	if err != nil {
+		return nil, http.StatusInternalServerError, errors.Wrap(err, "failed to fetch eligible schemes from database")
+	}
+	schemeListView := views.SchemeListViewFrom(schemes)
+
+	data, err := json.EncodeView(schemeListView)
+	if err != nil {
+		return nil, http.StatusInternalServerError, errors.Wrap(err, fmt.Sprintf(handlers.ErrEncodeView, listHandlerName))
+	}
+
+	return data, http.StatusOK, nil
 }
